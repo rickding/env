a8af580b6f59636ccb73d331d24eac40ba5178ee
dingxl@u1 <dingxl18@qq.com>
2018-10-26 21:42:10 +0800

    Update env

--- env.sh
+++ env.sh
@@ -3,12 +3,8 @@
 apt-get autoremove -y
 apt-get update
 
-# curl
-which curl
-apt-get install curl
-
 # docker
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 
@@ -24,10 +20,15 @@ apt-get -y install docker-compose
 alias cmps=docker-compose
 
 # python3
-apt-get install python3
+apt-get -y install python3
 update-alternatives --install /usr/bin/python python /usr/bin/python2 100
 update-alternatives --install /usr/bin/python python /usr/bin/python3 150
 
 # pip
-apt-get install python3-pip
+apt-get -y install python3-pip
 python -m pip install -i http://pypi.douban.com/simple --trusted-host pypi.douban.com --upgrade pip
+pip -V
+
+# git
+git config --global user.email "dingxl18@qq.com"
+git config --global user.name "dingxl"
