3d1d29185cc1f48c5458575aadf6c4022edc82f1
dingxl <dingxl@dds.com.cn>
2018-11-14 18:08:30 +0800

    Update apt sources

--- env.sh
+++ env.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-apt-get autoremove -y
-apt-get update
+apt autoremove -y
+apt update
+apt -y install software-properties-common
+
+# specified repositories or gedit /etc/apt/sources.list
+add-apt-repository "deb https://mirrors.ustc.edu.cn/ubuntu/ xenial main restricted universe multiverse"
+add-apt-repository "deb http://mirrors.163.com/ubuntu/ precise main universe restricted multiverse"
+apt update
 
 
 # docker
-apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+apt -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  xenial  stable"
-apt-get update
-apt-get -y install docker-ce
+apt update
+apt -y install docker-ce
 
 # docker from snap
 # snap install docker
 
 
 # python3
-apt-get -y install python3
+apt -y install python3
 update-alternatives --install /usr/bin/python python /usr/bin/python2 100
 update-alternatives --install /usr/bin/python python /usr/bin/python3 150
 
 # pip
-apt-get -y install python3-pip
+apt -y install python3-pip
 python -m pip install -i http://pypi.douban.com/simple --trusted-host pypi.douban.com --upgrade pip
 pip -V
 
