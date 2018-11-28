addb7c35ee8336f913f3b718032c382b95882a3d
dingxl <dingxl>
2018-11-29 07:08:29 +0800

    win

---
+++ env.bat
@@ -0,0 +1,7 @@
+rem vcpkg: https://github.com/Microsoft/vcpkg
+rem git clone https://github.com/Microsoft/vcpkg.git
+
+rem cygwin: https://www.aliyun.com/jiaocheng/921219.html
+rem http://cygwin.com/setup.exe
+
+rem maven
