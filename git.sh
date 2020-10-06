d93d4c2871d50c6cf8bbaf329395d63e62698297
dingxl <dingxl@xsheep.vip>
2020-10-06 18:09:18 +0800

    update node registry

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 cat ~/.ssh/id_rsa.pub
 
 # add key
-ssh-keygen -t rsa -C "dingxl18@qq.com"
+ssh-keygen -t rsa -C "dingxl@xsheep.vip"
 
 cat ~/.ssh/id_rsa.pub
 clip < ~/.ssh/id_rsa.pub
@@ -11,7 +11,8 @@ pbcopy < ~/.ssh/id_rsa.pub
 
 ssh -T git@gitee.com
 ssh -T git@github.com
+ssh -T git@code.aliyun.com
 
 # config user
-git config --global user.email "dingxl18@qq.com"
+git config --global user.email "dingxl@xsheep.vip"
 git config --global user.name "dingxl"
--- node.sh
+++ node.sh
@@ -4,3 +4,10 @@ apt -y install npm
 
 # node with cnpm: http://npm.taobao.org/
 npm install -g cnpm --registry=https://registry.npm.taobao.org
+
+# specify registry
+npm add webpack@latest --registry=https://registry.npm.taobao.org
+
+npm install  --registry=https://registry.npm.taobao.org
+
+npm run build
