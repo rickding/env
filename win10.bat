5f417f1ed600b8d2e3ff7aeeaadc00fbd0b109a4
dingxl <dingxl@xsheep.vip>
2021-02-25 18:45:49 +0800

    update

--- win10.bat
+++ win10.bat
@@ -3,3 +3,6 @@ dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux 
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 wsl --set-default-version 2
+
+## 增加开机启动
+C:\Users\pc1\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
