808a260a091fe434197e4be24b7f18e7d5f2e8ea
dingxl <dingxl18@qq.com>
2021-07-07 17:58:56 +0800

    增加拉取代码判断

--- fetch_multi.sh
+++ fetch_multi.sh
@@ -3,11 +3,16 @@
 for REPO in api admin deploy pc app env doc HelloDocker HelloJava HelloPython StarterAdmin StarterApi StarterDeploy toutiao
 do
     echo "fetch: " $REPO
-    cd $REPO
 
-	git fetch origin
+	if [ ! -d "$REPO" ]; then
+		git clone ssh://git@10.10.220.154:30001/e-tron/$REPO.git
+	else
+		cd $REPO
 
-	git checkout master && git pull
+		git fetch origin
 
-    cd ..
+		git checkout master && git pull
+
+		cd ..		
+	fi
 done
