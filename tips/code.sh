6b3722649158fbc22247ef3ddd0dd3232479f208
dingxl <dingxianli@gdairport.com>
2022-06-24 14:37:04 +0800

    代码行数统计工具，调整命令行参数，和工具结果基本一致，命令行控制代码扩展名称更灵活

--- tips/code.sh
+++ tips/code.sh
@@ -3,4 +3,14 @@
 # 使用工具统计代码行数，两者得到的数据一致，VSCode Counter，IDEA Statistic，https://www.jianshu.com/p/1a08c3dcbe42
 
 # 命令行统计代码行数，包含空行和注释时得到的数据和工具基本一致，https://blog.csdn.net/h784707460/article/details/95508796
-find . "(" -name "*.java" -or -name "*.js" -or -name "*.vue" -or -name "*.cs" -or -name "*.h" -or -name "*.cpp" -or -name "*.py" -or -name "*.sh" -or -name "*.yml" ")" -print | xargs grep -v "^$" | wc -l
+
+CODE_DIR=$1
+
+if [ -z "$CODE_DIR" ]
+then
+	CODE_DIR="./"
+fi
+
+echo "代码目录：$CODE_DIR"
+
+find $CODE_DIR "(" -name "*.java" -or -name "*.js" -or -name "*.vue" -or -name "*.cs" -or -name "*.h" -or -name "*.cpp" -or -name "*.py" -or -name "*.sh" -or -name "*.yml" ")" -print | xargs grep -v "^$" | wc -l
