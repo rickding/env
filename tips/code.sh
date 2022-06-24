a7f2ad886862a5f938310dbd59af35e452894dc7
dingxl <dingxianli@gdairport.com>
2022-06-24 14:25:49 +0800

    统计代码行数工具：vscode counter, idea statistic“

--- tips/code.sh
+++ tips/code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# https://blog.csdn.net/h784707460/article/details/95508796
+# 使用工具统计代码行数，两者得到的数据一致，VSCode Counter，IDEA Statistic，https://www.jianshu.com/p/1a08c3dcbe42
 
-# 统计代码行数，包括注释，不包括空行
-find . "(" -name "*.java" -or -name "*.js" -or -name "*.vue" -or -name "*.cs" -or -name "*.h" -or -name "*.cpp" -or -name "*.py" -or -name "*.sh" ")" -print | xargs grep -v "^$"|wc -l
+# 命令行统计代码行数，包含空行和注释时得到的数据和工具基本一致，https://blog.csdn.net/h784707460/article/details/95508796
+find . "(" -name "*.java" -or -name "*.js" -or -name "*.vue" -or -name "*.cs" -or -name "*.h" -or -name "*.cpp" -or -name "*.py" -or -name "*.sh" -or -name "*.yml" ")" -print | xargs grep -v "^$" | wc -l
